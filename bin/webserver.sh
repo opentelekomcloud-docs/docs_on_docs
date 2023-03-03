22ec9aaf990cf9f35c52672b9ec04054d728c730
Nils Magnus <nils.magnus@t-systems.com>
2023-03-03 23:59:32 +0000

    add micro webserver

--- bin/webserver.sh
+++ bin/webserver.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+port=5000
+
+while sudo lsof -i4:$port -sTCP:LISTEN -t; do
+    port=$((port + 1))
+done
 
 cd doc/build/html
-python -m http.server 5000
+
+ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+echo "Open web browser at http://$ip:$port/"
+python -m http.server $port

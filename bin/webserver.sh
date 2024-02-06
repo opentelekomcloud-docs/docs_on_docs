8d82992ec610128bc45cf634a3b995e4c12c7aab
Nils Magnus <magnus@linuxtag.org>
2024-02-06 15:35:40 +0100

    Update webserver.sh

--- bin/webserver.sh
+++ bin/webserver.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
+#
+# serve some local content (which has been created with "tox -e docs"), this
+# convenience script autodetects a free port starting from 5000, and prints
+# out the URL to this local site.
+#
 port=5000
-
 while sudo lsof -i4:$port -sTCP:LISTEN -t; do
     port=$((port + 1))
 done
 
 cd doc/build/html
 
+# autodetect EIP via meta data service:
 ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
 echo "Open web browser at http://$ip:$port/"
